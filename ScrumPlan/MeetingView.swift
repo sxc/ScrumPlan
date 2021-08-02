828907df8b7f9c2f68f801b58ad8bf4493a0fd48
sxc <qiuyequ@icloud.com>
2021-08-02 15:10:51 +0800

    add meeting view

---
+++ ScrumPlan/MeetingView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  ScrumPlan
+//
+//  Created by Xiaochun Shen on 2021/8/2.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack{
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.tophalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel(Text("Time remaining"))
+            .accessibilityValue(Text("10 minutes"))
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            
+            HStack {
+                Text("Speak 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+            }
+                .accessibilityLabel(Text("Next speaker"))
+        }
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
--- ScrumPlan/ScrumPlanApp.swift
+++ ScrumPlan/ScrumPlanApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ScrumPlanApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MeetingView()
         }
     }
 }
