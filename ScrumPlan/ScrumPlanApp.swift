ea550159ed8899eb7e9f76dcdb29c2ed4a88bdf3
sxc <qiuyequ@icloud.com>
2021-08-02 14:53:00 +0800

    Initial Commit

---
+++ ScrumPlan/ScrumPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumPlanApp.swift
+//  ScrumPlan
+//
+//  Created by Xiaochun Shen on 2021/8/2.
+//
+
+import SwiftUI
+
+@main
+struct ScrumPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
