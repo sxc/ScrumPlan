13ea4bdd6709f37d3665135d23aa3595917bae99
sxc <qiuyequ@icloud.com>
2021-08-03 09:11:39 +0800

    add scrums view

---
+++ ScrumPlan/ScrumsView.swift
@@ -0,0 +1,26 @@
+//
+//  ScrumsView.swift
+//  ScrumsView
+//
+//  Created by Xiaochun Shen on 2021/8/3.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    var body: some View {
+        List {
+            ForEach(scrums, id: \.title) { scrum in
+                CardView(scrum: scrum)
+                    .listRowBackground(scrum.color)
+            }
+        }
+    }
+}
+
+struct ScrumsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrumsView(scrums: DailyScrum.data)
+    }
+}
